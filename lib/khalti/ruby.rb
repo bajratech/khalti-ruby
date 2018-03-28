b8f362686660efd862da2f7311b4df90840c9f8d
Degendra Sivakoti <degendra.sivakoti@gmail.com>
2018-03-28 21:21:00 +0545

    initial gem setup

---
+++ lib/khalti/ruby.rb
@@ -0,0 +1,7 @@
+require "khalti/ruby/version"
+
+module Khalti
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ lib/khalti/ruby/version.rb
@@ -0,0 +1,5 @@
+module Khalti
+  module Ruby
+    VERSION = "0.1.0"
+  end
+end
