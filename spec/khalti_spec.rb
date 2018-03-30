7c2d71ec20f2f806971ab426b6231c42b9fbefd5
Degendra Sivakoti <degendra.sivakoti@gmail.com>
2018-03-30 18:10:44 +0545

    rspec added to check class and its methods existance

--- spec/khalti_spec.rb
+++ spec/khalti_spec.rb
@@ -1,9 +1,28 @@
 RSpec.describe Khalti do
+  let(:transaction) {Khalti::Transaction.new}
+  let(:verification) {Khalti::Verification.new}
+
   it "has a version number" do
     expect(Khalti::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "has transaction class" do
+    expect(transaction).to be_a Khalti::Transaction
+  end
+
+  it "has all method in transaction class" do
+    expect(Khalti::Transaction).to respond_to(:all)
+  end
+
+  it "has find method in transaction class" do
+    expect(Khalti::Transaction).to respond_to(:find).with(1).argument
+  end
+
+  it "has verification class" do
+    expect(verification).to be_a Khalti::Verification
+  end
+
+  it "has verify method in verification class" do
+    expect(Khalti::Verification).to respond_to(:verify).with(2).arguments
   end
 end
