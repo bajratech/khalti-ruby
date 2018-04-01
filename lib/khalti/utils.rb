54ca4eef4e842b9e1f63892eafa67a316ef5683d
Dhurba Baral <dhurba87@gmail.com>
2018-04-02 03:35:34 +0545

    creates utils for common helper function

---
+++ lib/khalti/utils.rb
@@ -0,0 +1,20 @@
+module Khalti
+  module Util
+    def self.raise_exception_for_status_code(status_code, message=nil)
+      case status_code.to_i
+      when 401
+        raise AuthenticationError
+      when 403
+        raise AuthorizationError, message
+      when 404
+        raise NotFoundError
+      when 500
+        raise ServerError
+      when 503
+        raise DownForMaintenanceError
+      else
+        raise UnexpectedError "Unexpected HTTP_RESPONSE #{status_code.to_i}"
+      end
+    end
+  end
+end
