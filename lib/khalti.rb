718c300778addc2274e04c465d79a39f8d7b95f0
Degendra Sivakoti <degendra.sivakoti@gmail.com>
2018-04-01 13:17:20 +0545

    basic code refactoring

--- lib/khalti.rb
+++ lib/khalti.rb
@@ -1,6 +1,7 @@
 require_relative "khalti/version"
 require_relative "khalti/verification"
 require_relative "khalti/transaction"
+require_relative "khalti/request_helper"
 
 module Khalti
   # Your code goes here...
