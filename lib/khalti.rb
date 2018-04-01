68ab590f0a9cef5a4381f8ab9daebb8f6136235e
Dhurba Baral <dhurba87@gmail.com>
2018-04-02 03:36:59 +0545

    import utils and exceptions file

--- lib/khalti.rb
+++ lib/khalti.rb
@@ -1,8 +1,8 @@
 require_relative "khalti/version"
+
+require_relative 'khalti/exceptions'
+require_relative 'khalti/utils'
+
 require_relative "khalti/verification"
 require_relative "khalti/transaction"
 require_relative "khalti/request_helper"
-
-module Khalti
-  # Your code goes here...
-end
